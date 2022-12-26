2599eb64a4969678d1abccb6824073f009ddadc9
Egor <felbushe@gmail.com>
2022-12-27 04:54:23 +0500

    Telegram bot

---
+++ Python/TelegramBot/config.py
@@ -0,0 +1,10 @@
+TOKEN = '5968728906:AAFEvIyLC-IuBd1Ict2sWkD4rXyCuZ0Hhug'
+
+keys = {
+    'евро': 'EUR',
+    'euro': 'EUR',
+    'доллар': 'USD',
+    'dollar': 'USD',
+    'рубль': 'RUB',
+    'rub': 'RUB',
+}
