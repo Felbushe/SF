e464e3e4f0b65fe39156846c3bdf4808db5c93d2
Egor <felbushe@gmail.com>
2022-12-27 18:18:53 +0500

    boom brain

--- Python/TelegramBot/extension.py
+++ Python/TelegramBot/extension.py
@@ -29,4 +29,4 @@ class Converter:
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = float(json.loads(r.content)[keys[base]])
 
-        return total_base
+        return float(total_base * amount)
